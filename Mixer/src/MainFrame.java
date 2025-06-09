2bde9418fef7c7e6f97f7abc9d5a882173eab9eb
Applepielord <pixeldash2007@gmail.com>
2025-06-09 16:46:15 -0400

    MainFrame components
    added ingredient list

--- Mixer/src/MainFrame.java
+++ Mixer/src/MainFrame.java
@@ -10,18 +10,31 @@ public class MainFrame {
     //This Method displays the Frame
     //ONLY CALL ON THIS FUNCTION WHEN YOU NEED THE FRAME TO BE DISPLAYED 
     public void DisplayMainFrame() throws Exception {
+        //Essential Components
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
+        //lists
+        JTextPane ingredientList = new JTextPane();
+
+        //Scrollbars
+        JScrollPane ingredientListScroll = new JScrollPane(ingredientList);
+
+        //essential frame display stuff
         frame.setResizable(false);
-        frame.setSize(1000, 700);
+        frame.setSize(1200, 700);
         frame.setLocationRelativeTo(null);
         panel.setLayout(null);
-        panel.setSize(900, 625);
+        panel.setSize(1200, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Mixer");
+        frame.setTitle("The Cooking Station");
 
+        //Setting component positions
+        ingredientListScroll.setBounds(40, 40, 300, 500);
+
+        //adding components to the frame
         frame.add(panel);
+        panel.add(ingredientListScroll);
         frame.setVisible(true);
     }
 }
