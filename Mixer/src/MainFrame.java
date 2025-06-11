de1ba14a1fa5b71fce7ae6573104a3d5bd1e64b1
Applepielord <pixeldash2007@gmail.com>
2025-06-11 12:04:46 -0400

    database
    idk why its telling me to commit this..

--- Mixer/src/MainFrame.java
+++ Mixer/src/MainFrame.java
@@ -145,20 +145,7 @@ public class MainFrame {
     //this will be used to initialize the connection for databases
     private Connection initConnection()
     {
-    // Function made by Shannon Duldulao
-    //this is used for Connecting the database into the program
-    try {
-        Connection Connect = DriverManager.getConnection(dbUrl);
-        Statement S = Connect.createStatement();
-        S.execute("SELECT * FROM Ingredients");
-        ResultSet Rs = S.getResultSet();
-        System.out.println(Rs.getString(1)+ " " + Rs.getString(2));
-        return Connect;
-    }
-    catch (SQLException E){
         return null;
-    }
-
     }
     //used to reload the recipe list
     //call this function whenever user makes changes to ingredients
