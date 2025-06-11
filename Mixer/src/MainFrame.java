4a419c088c6df97ad6167f1b6616e05eca790595
Applepielord <pixeldash2007@gmail.com>
2025-06-11 10:53:46 -0400

    Update
    Added more program stuff

--- Mixer/src/MainFrame.java
+++ Mixer/src/MainFrame.java
@@ -2,7 +2,15 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.geom.AffineTransform;
 import java.sql.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /* Script created by Nigel Garcia
  * June 9 2025
@@ -13,6 +21,7 @@ import java.sql.*;
 public class MainFrame {
     //dont forget to change this value to the actual path once the database is setup
     private final String dbUrl = "jdbc:ucanaccess://Mixer\\Database\\Tool Mixer.accdb";
+    private JButton[] ingredientListBtn = new JButton[40]; //have arraysize change depending on database elements
 
     //This Method displays the Frame
     //ONLY CALL ON THIS FUNCTION WHEN YOU NEED THE FRAME TO BE DISPLAYED 
@@ -34,6 +43,7 @@ public class MainFrame {
         JScrollPane recipeListScroll = new JScrollPane(recipeList);
 
         //ScrollBars
+        JScrollBar sb = new JScrollBar();
         JScrollBar ingredScrollBar = ingredientListScroll.getVerticalScrollBar();
         ingredScrollBar.setUI(new ScrollBarVisual());
 
@@ -43,6 +53,8 @@ public class MainFrame {
         JButton addRecipeBtn = new JButton("Add Recipe");
         JButton exitBtn = new JButton("Exit");
         JButton startBtn = new JButton("Start");
+
+        
 
         //Labels
         JLabel bgAnim = new JLabel(new ImageIcon("Mixer\\Graphics\\Background\\MainFramebackGround.gif"));
@@ -56,7 +68,8 @@ public class MainFrame {
         frame.setSize(1200, 700);
         frame.setLocationRelativeTo(null);
         panel.setLayout(null);
-        ingredientList.setLayout(new BoxLayout(ingredientList, BoxLayout.Y_AXIS));
+        //ingredientList.setLayout(new BoxLayout(ingredientList, BoxLayout.Y_AXIS));
+        ingredientList.setLayout(null);
         recipeList.setLayout(null);
         panel.setSize(1200, 700);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -66,7 +79,9 @@ public class MainFrame {
         addIngredientBtn.addActionListener(e -> userAddIngredient());
         exitBtn.addActionListener(e -> userClickedExit(frame));
         addRecipeBtn.addActionListener(e -> userAddRecipe());
-        startBtn.addActionListener(e -> userClickedStart());
+        //startBtn.addActionListener(e -> userClickedStart());
+        startBtn.addActionListener(e -> rotateComponents(panel, bgAnim, ingredientListBtn));
+        sb.addAdjustmentListener(new scrollListener(ingredientListBtn));
 
         //Setting component positions
         ingredientListScroll.setBounds(40, 40, 300, 500);
@@ -74,22 +89,33 @@ public class MainFrame {
         exitBtn.setBounds(525, 600, 125, 25);
         recipeListScroll.setBounds(850, 40, 300, 500);
         addRecipeBtn.setBounds(900, 550, 200, 25);
-        startBtn.setBounds(512, 560, 150, 30);
+        startBtn.setBounds(512, 0, 150, 30);
         bgAnim.setBounds(0, 0, 1200, 700);
+        sb.setBounds(400, 300, 35, 300);
 
         //adding components to the frame
         frame.add(panel);
-        panel.add(ingredientListScroll);
+        //panel.add(ingredientListScroll);
         panel.add(addIngredientBtn);
         panel.add(exitBtn);
-        panel.add(recipeListScroll);
+        //panel.add(recipeListScroll);
         panel.add(addRecipeBtn);
         panel.add(startBtn);
         reloadIngredients(ingredientList);
-        
+        panel.add(sb);
 
         panel.add(bgAnim);
         frame.setVisible(true);
+    }
+    private void rotateComponents(JPanel ingredientList, JLabel bgAnim, JButton[] ingredientListBtn)
+    {
+        Timer timer = new Timer(1/10000, new ButtonRotation(ingredientList, bgAnim, ingredientListBtn));
+        timer.start();
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.schedule(() -> {
+            timer.stop();
+        }, 1, TimeUnit.SECONDS);
+        scheduledExecutorService.shutdown();
     }
     private void userAddIngredient()
     {
@@ -131,7 +157,7 @@ public class MainFrame {
     //call this function whenever user makes changes to ingredients
     private void reloadIngredients(JPanel ingredientList)
     {
-        JButton[] ingredBtn = new JButton[40];
+        /*JButton[] ingredBtn = new JButton[40];
         for (int i = 0; i < ingredBtn.length; i++)
         {
             ingredBtn[i] = new JButton("Test!");
@@ -139,7 +165,80 @@ public class MainFrame {
             ingredientList.add(ingredBtn[i]);
             ingredientList.revalidate();
             ingredientList.repaint();
+        }*/
+    }
+}
+class scrollListener implements AdjustmentListener
+{
+    private int angle = 2;
+    private JButton[] ingredBtn = null;
+    int j = 0;
+    int offset = 50;
+    public scrollListener(JButton[] btnList)
+    {
+        if (btnList != null)
+        {
+            ingredBtn = btnList;
         }
+    }
+    int current = 1;
+    public void adjustmentValueChanged(AdjustmentEvent e)
+    {
+        if (e.getValue() < current) //up
+        {
+            for (int j = 0; j < ingredBtn.length && ingredBtn[j] != null; j++)
+            {
+                current = e.getValue();
+                ingredBtn[j].setBounds(2 + ingredBtn[j].getBounds().x, ingredBtn[j].getBounds().y - 10, 300, 35);
+                offset += 50;
+            }
+        }
+        else if (e.getValue() > current)
+        {
+            for (int j = 0; j < ingredBtn.length && ingredBtn[j] != null; j++)
+            {
+                current = e.getValue();
+                ingredBtn[j].setBounds(-2 + ingredBtn[j].getBounds().x, 10 + ingredBtn[j].getBounds().y, 300, 35);
+                offset += 50;
+            }
+        }
+    }
+}
+class ButtonRotation implements ActionListener
+{
+    JButton[] ingredBtn = null;
+    private JPanel ingredientList = null;
+    private JLabel bgAnim = null;
+    int i = 0;
+    int j = 0;
+    int offset = 50;
+    public ButtonRotation(JPanel ingredList, JLabel bg, JButton[] ingredientBtn)
+    {
+        ingredientList = ingredList;
+        bgAnim = bg;
+        ingredBtn = ingredientBtn;
+    }
+    MainFrame mainFrame = new MainFrame();
+    int angle = 0;
+    @Override
+    public void actionPerformed(ActionEvent arg0)
+    {
+        
+        if (i < ingredBtn.length-1)
+        {
+            ingredientList.remove(bgAnim);
+            ingredBtn[i] = new JButton("Test!");
+            ingredBtn[i].setBounds((-offset/5) + 100, offset - 150, 300, 35);
+            ingredientList.add(ingredBtn[i]);
+            ingredientList.revalidate();
+            ingredientList.repaint();
+            i++;
+            offset += 50;
+            ingredientList.add(bgAnim);
+            ingredientList.revalidate();
+            ingredientList.repaint();
+        }
+        
     }
 }
 class ScrollBarVisual extends BasicScrollBarUI
