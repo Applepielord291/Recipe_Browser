0e5e3d3db32310e0088e94b9f3105ffcd218bfe9
Applepielord <pixeldash2007@gmail.com>
2025-06-17 13:30:20 -0400

    Update

--- Mixer/src/IngredientInfoFrame.java
+++ Mixer/src/IngredientInfoFrame.java
@@ -9,15 +9,19 @@ public class IngredientInfoFrame {
         ImageIcon exitNormal = new ImageIcon("Mixer\\Graphics\\Buttons\\ExitNormal.png");
         ImageIcon exitHovered = new ImageIcon("Mixer\\Graphics\\Buttons\\ExitHovered.gif");
         ImageIcon recipeNameIcon = new ImageIcon("Mixer\\Graphics\\Labels\\RecipeName.png");
+        ImageIcon recipeInstructions = new ImageIcon("Mixer\\Graphics\\Labels\\Instructions.png");
         JDialog frame = new JDialog();
         JPanel panel = new JPanel();
 
         JLabel nameTitleLbl = new JLabel(recipeNameIcon);
         JLabel recipeName = new JLabel(btn.getText());
-        JLabel recipeInstructionsLbl = new JLabel("Recipe Instructions");
-        JLabel recipeRequirements = new JLabel(getIng(con, btn));
+        JLabel recipeInstructionsLbl = new JLabel(recipeInstructions);
+        JTextPane recipeRequirements = new JTextPane();
+        JScrollPane recipeRequirementsScroll = new JScrollPane(recipeRequirements);
+        recipeRequirements.setText(getIng(con, btn));
 
         recipeName.setFont(new Font("Arial", 0, 20));
+        recipeRequirements.setFont(new Font("Arial", 0, 20));
 
         JButton exitBtn = new JButton(exitNormal);
         exitBtn.setRolloverIcon(exitHovered);
@@ -38,16 +42,17 @@ public class IngredientInfoFrame {
         exitBtn.addActionListener(e -> userClickedExit(frame));
 
         nameTitleLbl.setBounds(10, 10, 250, 45);
-        recipeName.setBounds(10, 60, 150, 25);
-        recipeRequirements.setBounds(150, 10, 150, 25);
-        recipeInstructionsLbl.setBounds(90, 10, 250, 45);
+        recipeName.setBounds(20, 70, 300, 25);
+        recipeRequirementsScroll.setBounds(340, 70, 300, 150);
+        recipeInstructionsLbl.setBounds(300, 10, 250, 45);
         exitBtn.setBounds(275, 450, 125, 25);
 
         frame.add(panel);
+
         panel.add(nameTitleLbl);
         panel.add(recipeName);
         panel.add(recipeInstructionsLbl);
-        panel.add(recipeRequirements);
+        panel.add(recipeRequirementsScroll);
         panel.add(exitBtn);
 
         frame.setVisible(true);
@@ -67,12 +72,14 @@ public class IngredientInfoFrame {
             ResultSet rs = ps.executeQuery();
             if (rs.next())
             {
-                return rs.getString(3);
+                System.out.println(rs.getString(6));
+                return rs.getString(6);
             }
             else return null;
         }
         catch (Exception e)
         {
+            e.printStackTrace();
             return null;
         }
     }
